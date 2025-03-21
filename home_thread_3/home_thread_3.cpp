4e57cdd8ee983872f8623f5440330f9760f5595e
Alexandr Gromov <gromovaleksandr813@gmail.com>
2025-03-21 23:53:31 +0300

    Initial

---
+++ home_thread_3/home_thread_3.cpp
@@ -0,0 +1,43 @@
+﻿#include <chrono>
+#include <future>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+
+void selectionSort(vector<int>& array, promise<void> pr) {
+	int size = array.size();
+	int  min;
+	for (int i = 0; i < size - 1; i++) {
+		min = i; 
+		for (int j = i + 1; j < size; j++) {
+			
+			if (array[min] > array[j]) {
+			
+				min = j; 
+			}
+		}
+		int temp = array[i]; 
+		array[i] = array[min]; 
+		array[min] = temp; 
+		
+	}
+	this_thread::sleep_for(1000ms);
+        pr.set_value();
+}
+
+
+int main(){
+	vector<int> arr1{2, 4, 56, 23, 12, 78,59,34};
+	promise<void> pr;
+	future<void> fut = pr.get_future();
+	auto t1 = async(selectionSort, ref(arr1), move(pr));
+	fut.wait();
+	for (auto i:arr1){
+
+		cout << i << endl;
+	}
+	return EXIT_SUCCESS;
+}
---
+++ home_thread_3_2/home_thread_3_2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+#include <future>
+
+using namespace std;
+template<typename T, typename F>
+void for_each_parallel(T begin, T end, F func){
+    auto Size = end - begin;
+	if (Size <= 1){
+		func(*begin);
+		return;
+	}
+	auto midle = begin;
+	midle+=1;
+	auto R = async(for_each_parallel<T,F>, begin, midle, func);
+    for_each_parallel(midle, end, func);
+}
+
+void printVector(vector<int> v){
+	cout << "Container: { ";
+	for (auto i:v){
+		cout << i << " ";
+	}
+   cout << "};" << endl;
+}
+
+int main(){ 
+	int N = 10;
+	vector<int> v{};
+	for (int i = 0; i < N; ++i){
+		v.push_back(rand() % 30);
+	}
+	printVector(v);
+	auto f = [](int& n) {n*=2; };
+	for_each_parallel(v.begin(), v.end(), f);
+	printVector(v);
+	return EXIT_SUCCESS;
+}
